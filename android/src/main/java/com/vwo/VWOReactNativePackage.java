06284a175895603519d572c7a1142be7e9663f2f
Amandeep Anguralla <amandeep.anguralla@wingify.com>
2017-10-13 12:01:09 +0530

    Downgraded library versions

--- android/src/main/java/com/vwo/VWOReactNativePackage.java
+++ android/src/main/java/com/vwo/VWOReactNativePackage.java
@@ -23,10 +23,10 @@ public class VWOReactNativePackage implements ReactPackage {
      * listed here. Also listing a native module here doesn't imply that the JS implementation of it
      * will be automatically included in the JS bundle.
      */
-    @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-      return Collections.emptyList();
-    }
+//    @Override
+//    public List<Class<? extends JavaScriptModule>> createJSModules() {
+//      return Collections.emptyList();
+//    }
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
